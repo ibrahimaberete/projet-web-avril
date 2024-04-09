7477b2441d86e324672868177c4d69c8617792a8
Jeremy Weltmann <jeremy.we@hotmail.fr>
2024-04-09 09:10:19 +0200

    add-city-page-components

--- flutter_firebase_app/lib/ImageListPage.dart
+++ flutter_firebase_app/lib/ImageListPage.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'AddCityPage.dart';
 
 class ImageListPage extends StatelessWidget {
+  const ImageListPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,50 +54,6 @@ class ImageListPage extends StatelessWidget {
             },
           );
         },
-      ),
-    );
-  }
-}
-
-class AddCityPage extends StatelessWidget {
-  final TextEditingController urlController = TextEditingController();
-  final TextEditingController nameController = TextEditingController();
-
-  AddCityPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        
-        title: const Text('Ajouter une ville'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: 'Nom de la ville'),
-            ),
-            TextField(
-              controller: urlController,
-              decoration: const InputDecoration(labelText: 'Image URL'),
-            ),
-            ElevatedButton(
-              child: const Text('Ajouter'),
-              onPressed: () {
-                FirebaseFirestore.instance.collection('carousel').doc(nameController.text).set({
-                  'img': urlController.text,
-                  'id': nameController.text,
-                  'likes': 0
-                });
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
