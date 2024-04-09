10032b49d18e64cbc360ef191cdc8ec573470f09
ibrahimaberete <ibrahima.berete18@gmail.com>
2024-04-09 12:02:13 +0200

    update fluuter and angular ok

--- web-firebase/src/app/components/city-dialog/city-dialog.component.ts
+++ web-firebase/src/app/components/city-dialog/city-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, Inject, signal } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CarouselService } from 'src/app/services/carousel.service';
 
 @Component({
   selector: 'app-city-dialog',
@@ -15,6 +16,7 @@ export class CityDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<CityDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private carouselService: CarouselService,
     private fb: FormBuilder
   ) {
     this.cityForm = this.fb.group({
@@ -39,16 +41,31 @@ export class CityDialogComponent {
       const imageUrl = this.cityForm.get('imageUrl')?.value;
       const like = this.cityForm.get('like')?.value;
 
+      // Vérifiez que cityName n'est pas vide
+      if (!cityName) {
+        alert('Le nom de la ville ne peut pas être vide');
+        return;
+      }
+
       const cityData = { id: cityName, img: imageUrl, likes: like };
+
+      // Afficher les valeurs de this.data.id et cityName
+      console.log('this.data.id:', this.data.id);
+      console.log('cityName:', cityName);
+
+      // Fournir oldId et newId lors de l'appel à update
+      this.carouselService.update({ oldId: this.data.id, newId: cityName, ...cityData });
 
       this.dialogRef?.close(cityData);
     }
   }
-
   onClose(): void {
     if (this.cityForm) {
     this.dialogRef?.close();
     }
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
